4e01fe346f8eef992a23819b65f0903b018e7245
Dionta Jones <diontajones@gmail.com>
2023-03-29 20:47:52 -0400

    adjusted user input

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import checkers
 
 
 def game():
-    size = int(input("Enter the size of the board: "))
+    have_board = False
+    while not have_board:
+        print("Enter the size of the board - (min: 4, max: 16 ")
+        size = int(input('> '))
+        if size < 4 or size > 16:
+            print('Please enter a number between 4 and 16.')
+        else:
+            have_board = True
+
     board = checkers.build_board(size)
     print(board)
     print("Number of Empty cells:", checkers.get_count(board, 'Empty'))
